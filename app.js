865ef1fef8ccfb2ddf8548b1f28acd49d3d5fd65
ericsei <eric.ahhpusei@gmail.com>
2020-12-20 14:18:47 -0500

    prepare for deploy

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const path = require("path");
 const expressValidator = require("express-validator");
 const port = process.env.PORT || 8000;
 require("dotenv").config();
@@ -84,14 +85,14 @@ app.use("/api", productRoutes);
 app.use("/api", stockRoutes);
 app.use("/api", stripeRoutes);
 
-// for production use, we serve the static react build folder
+// Serve static assets in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "./client/build")));
+  // Set static folder
+  app.use(express.static("client/build"));
 
-  // all unknown routes should be handed to our react app
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "./client/build", "index.html"));
-  });
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
 }
 
 app.listen(port, (req, res) => {
