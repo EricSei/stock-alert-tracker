2d5b6f930d42e7c761dcf44a13119472579f2a8c
ericsei <eric.ahhpusei@gmail.com>
2020-12-20 12:00:48 -0500

    restrcture the code for deployment

--- zfs/client/src/components/Layout/Wrapper.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Container } from "semantic-ui-react";
-
-const Wrapper = ({ children }) => (
-  // style={{ marginTop: "15em" }} add some styles
-  <Container style={{ marginTop: "10em" }}>{children}</Container>
-);
-
-export default Wrapper;
--- zfs/client/src/pages/DailyUpdatePage/DailyUpdatePage.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Layout from "../../components/Layout/Layout";
-
-const DailyUpdate = () => {
-  return <Layout> Market Updates </Layout>;
-};
-
-export default DailyUpdate;
